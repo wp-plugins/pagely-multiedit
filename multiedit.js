9c77a04b00b5ceb41ac6cea9da160e97bc7af829
joshua.strebel <joshua.strebel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-14 18:31:20 +0000

    first commit page.ly multiedit
    git-svn-id: https://plugins.svn.wordpress.org/pagely-multiedit/trunk@205602 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ multiedit.js
@@ -0,0 +1,115 @@
+
+function initMultiEdit() {
+	if(jQuery('#multiEditHidden span').length > 1) {
+		jQuery('#multiEditControl').append(jQuery('#multiEditHidden span'));
+		jQuery('#postdivrich').before(jQuery('#multiEditControl'));
+		jQuery('#postdivrich').attr('rel','default');
+		
+		// hide the custome fields
+		
+		//shorten the span text and hide the custom fields
+		jQuery('#multiEditControl span').each( function(index) {
+			var oldstr = jQuery(this).html();
+			var newstr = oldstr.split('_');	
+			var metaid = jQuery(this).attr('rel');
+			if (newstr[1]) {
+				 jQuery(this).html(newstr[1]);
+			}
+			
+			// find and hide the tr for custom field
+			jQuery('tr#meta-'+metaid).addClass('mevalue').hide();
+		});
+		jQuery('table#list-table tbody').append('<tr id="multishow"><td class="left"><span>Show/Hide MultiEdit fields</span></td><td></td></tr>');
+	} else {
+		jQuery('#multiEditHidden').hide();
+		jQuery('#multiEditControl').hide();
+	}
+}
+
+function getTinyMCEContent() {
+	var iframeRef = jQuery('#content_ifr');
+	return jQuery(iframeRef).contents().find('body').html();
+}
+
+function toggleTinyMCE(newcontent) {
+	var iframeRef = jQuery('#content_ifr');
+	jQuery(iframeRef).contents().find('body').html(newcontent);
+
+}
+
+function copyDefaultToFreezer(content) {
+	jQuery('#multiEditFreezer').html(content);
+}
+function getFreezer() {
+	return jQuery('#multiEditFreezer').html();
+
+}
+function triggerSelected(idref,newid) {
+	jQuery('span.multieditbutton').removeClass('selected');
+	jQuery(idref).addClass('selected');
+	jQuery('#postdivrich').attr('rel',newid);
+}
+function getSelectedPostDiv() {
+	return jQuery('#postdivrich').attr('rel');
+}
+function getMetaContent(metaID) {
+	return jQuery("tr#meta-"+metaID+" td textarea").val();
+}
+function setMetaContent(metaID,string) {
+	return jQuery("tr#meta-"+metaID+" td textarea").val(string);
+}
+
+
+// ready
+jQuery(document).ready( function() {
+	
+	// if multiedit link is clicked lets store current tinymce string somwhere so we can retreive it later
+	// then retrieve stored string and place into tinyMCE 	
+	jQuery('span.multieditbutton').click(function() {
+		
+		// the tinyMCE iframe
+		var iframeRef = jQuery('#content_ifr');
+		// id of this multiedit 
+		var thisID = jQuery(this).attr('id');
+		var thisWPMetaID = jQuery(this).attr('rel');
+		
+		// get current rel attr #postdivrich that will tell use what content is inside of it
+		var selectedpostdiv = getSelectedPostDiv();	
+
+		// main the_content() tinymce
+		if(selectedpostdiv=="default") {
+			copyDefaultToFreezer(getTinyMCEContent());
+		} else {
+			// currently showing a multiedit region, lets update the meta textarea before switching	
+			var active = jQuery('span#'+selectedpostdiv).attr('rel');
+			setMetaContent(active,getTinyMCEContent());
+		}
+		
+	 	// Toggle the actual tinymce content
+		if (thisID == 'default') {	
+			// if default, grab content from freezer
+			toggleTinyMCE(getFreezer());
+		} else {
+			// grab the content from the meta field
+			toggleTinyMCE(getMetaContent(thisWPMetaID));
+			
+		}
+		
+		// trigger class changes for active button
+		triggerSelected(this,thisID);
+	});
+	
+	
+	// we want to catch the submit action to do some cleanup 
+	// and save the meta fields before wordpress posts the form
+	
+	jQuery('form#post').submit( function() {	
+		jQuery('span#default.multieditbutton').click();
+		jQuery('#postcustomstuff input.updatemeta').click();
+	});
+	
+	jQuery('table#list-table tr#multishow span').live( 'click' , function (){
+		jQuery('table#list-table tbody tr.mevalue').toggle();
+	});
+	initMultiEdit();
+});
---
+++ multiedit.php
@@ -0,0 +1,124 @@
+<?php
+/*
+Plugin Name: MultiEdit
+Plugin URI: http://blog.page.ly/multiedit-plugin
+Description: Multi Editable Region Support for Page Templates
+Version: 0.9
+Author: Joshua Strebel
+Author URI: http://page.ly
+
+*/
+
+function multiedit() {
+	add_action ('edit_page_form', 'multieditAdminEditor', 1);
+	add_action ('edit_form_advanced', 'multieditAdminEditor', 1);
+
+	add_action ('admin_head', 'testforMultiMeta', 1);
+
+	if (in_array(basename($_SERVER['PHP_SELF']),array('page-new.php','page.php')) ) {
+		add_action ('admin_head', 'multieditAdminHeader', 1);
+		//add_action ('save_post', 'multieditSavePost');
+	}
+}
+
+$GLOBALS['multiEditDisplay'] = false;
+// api for templates
+function multieditDisplay($index) {
+	if ($GLOBALS['multiEditDisplay'] === false) {
+		$GLOBALS['multiEditDisplay'] = get_post_custom(0);
+	}
+	$index = "multiedit_$index";	
+
+	if (isset($GLOBALS['multiEditDisplay'][$index])) {
+		echo $GLOBALS['multiEditDisplay'][$index][0];
+	}
+
+}
+
+
+/*function multieditRegionsForType($type) {
+
+	$regions = multieditCurrentThemeRegionMap();
+
+	if (isset($regions[$type])) {
+		return $regions[$type];
+	}
+	return array();
+}
+*/
+
+/*function multieditCurrentThemeRegionMap() {
+        $current_theme = get_current_theme();
+
+        $themes = get_themes();
+
+	$themeDir = $themes[$current_theme]['Template Dir'];
+
+	$regions = include ABSPATH.$themeDir.'/regions.php';
+
+	return $regions;
+}
+*/
+
+function multieditAdminHeader() {
+	echo '<link rel="stylesheet" type="text/css" href="' . get_settings('siteurl'). '/wp-content/plugins/multiedit/multiedit.css" />';	
+	echo '<script type="text/javascript" src="' . get_settings('siteurl'). '/wp-content/plugins/multiedit/multiedit.js" ></script>';	
+}
+
+function multieditAdminEditor() {
+	global $post;
+	echo '<div id="multiEditControl"></div>';
+	echo '<div id="multiEditHidden"><span class="multieditbutton selected" id="default">Main Content</span>';
+	if (isset($_GET['post'])) {
+		$meta = has_meta($_GET['post']);
+
+		if (is_array($meta)) {
+			foreach($meta as $item) {
+				if (preg_match('/^multiedit_(.+)/',$item['meta_key'])) {
+					echo "<span class='multieditbutton' id='hs_$item[meta_key]' rel='$item[meta_id]'>$item[meta_key]</span><input type='hidden' id='hs_$item[meta_key]' name='$item[meta_key]' value=\"".htmlspecialchars($item['meta_value']).'" />';
+				}
+			}
+		}
+	}
+	echo "<div id='multiEditFreezer' style='display:none'>".$post->post_content."</div></div>\n";
+}
+
+function testforMultiMeta() {
+	global $post;
+	////if (isset($_GET['post'])) {
+	$meta = has_meta($post->ID);
+	//print_r($meta);
+
+	// get current page template
+	$templatefile = locate_template(array($post->page_template));	
+	$template_data = implode('', array_slice(file($templatefile), 0, 10));	
+	$matches = '';
+	$added = false;
+	//check for multiedit declaration
+	if (preg_match( '|MultiEdit:(.*)$|mi', $template_data, $matches)) {
+		 $multi = explode(',',_cleanup_header_comment($matches[1]));
+		 
+		 //	echo $region;
+		 
+		 foreach($meta as $k=>$v) {
+		 	 foreach($multi as $region) {
+		 	  	if (in_array('multiedit_'.$region,$v)) {
+		 	  		$present[$region] = true;
+		 	  	}
+		 	 }
+		 }
+		 
+		foreach($multi as $region) {
+			if(!isset($present[$region])) {
+					update_post_meta($post->ID, 'multiedit_'.$region, '');
+
+			}
+		}
+				 
+	}
+		//update_post_meta($post->ID, 'multiedit_'.$region, '');
+		 
+
+}
+
+multiedit();
