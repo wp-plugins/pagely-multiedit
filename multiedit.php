1519702c5a0affb4223f5b8b09e63c9afa2506cd
joshua.strebel <joshua.strebel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-10 09:18:49 +0000

    0.9.8
    git-svn-id: https://plugins.svn.wordpress.org/pagely-multiedit/trunk@383130 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- multiedit.php
+++ multiedit.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Page.ly MultiEdit
 Plugin URI: http://blog.page.ly/multiedit-plugin
-Description: Multi-Editable Region Support for Page Templates BTYB. <a href="http://page.ly">Page.ly WordPress Hosting</a>
-Version: 0.9.7
-Author: Joshua Strebel
+Description: Multi-Editable Region Support for Page Templates. Brought to you by: <a href="http://page.ly">Page.ly WordPress Hosting</a>
+Version: 0.9.8
+Author: joshua.strebel 
 Author URI: http://page.ly
 */
 
@@ -42,6 +42,68 @@ Author URI: http://page.ly
  
 define ('PLUGINASSETS',WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'');
 
+// plugin option screen
+add_action('admin_menu','me_options');
+
+function me_options() {
+	add_options_page( 'Page.ly Multi-Edit Options', 'Multi-Edit', 'manage_options', 'multiedit-options', 'me_options_page');
+	multieditAdminHeader();
+
+	
+}
+
+function me_options_page() {
+	if (!current_user_can('manage_options'))
+    {
+      wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+    
+    $opts[] = array();
+    $opts['shortcodes'] = get_option( 'pagely_multiedit_shortcodes' );
+    !isset($opts['shortcodes']) ? update_option('pagely_multiedit_shortcodes',0) : '';
+    
+   // print_r($_POST);
+     if( isset($_POST['opts']['shortcode'])  ) {
+        // Read their posted value
+        $opts['shortcodes'] = $_POST['opts']['shortcode'];
+        // Save the posted value in the database
+        update_option( 'pagely_multiedit_shortcodes', $opts['shortcodes'] );
+     	?>
+			<div class="updated"><p><strong><?php _e('Multi-Edit settings saved.'); ?></strong></p></div>
+		<?php 
+     }
+
+	echo '<div class="wrap">';
+	echo "<h2>" . __( 'Page.ly Multi-Edit Options' ) . "</h2>";
+	?>
+		<div id="pme_split">
+
+			<div class="pme_left">
+				<form name='pme' method='post' action=''>
+				<table class='form-table'>
+					<tr>
+						<th><?php _e('Short Code Support:' ); ?></th>
+						<td><input type="radio" name="opts[shortcode]" value="1" <?= $opts['shortcodes'] == 1 ?  "checked='checked'" : ''?> /><label>Yes</label>  <input type="radio" name="opts[shortcode]" value="0" <?= $opts['shortcodes'] == 0 ?  "checked='checked'" : ''?>/><label>No</label> <br/><span class="description"><?php _e('Enables Shortcode support for multi-edit regions. Caution this will enable all filters for the region like including social media buttons and other such nonsense in your output.' ); ?></span></td>
+					</tr>
+					<tr>
+						<th></th>
+						<td><p class="submit">
+						<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+						</p></td>
+					</tr>
+				</table>
+				</form>
+			</div>
+			<div class="pme_right"><a href="http://page.ly" target="_blank"><img src="<?php echo PLUGINASSETS?>/pagely-plugin-ad.jpg"/></a></div>
+
+			<br style="clear:both"/>
+		</div>
+	<?php 
+	echo '</div>';
+}
+// end options screen
+
+
 if (in_array(basename($_SERVER['PHP_SELF']),array('post.php','page.php')) && $_GET['action'] == 'edit' ) {
 	add_action('init','multiedit');
 }
@@ -53,14 +115,28 @@ function multiedit() {
 $GLOBALS['multiEditDisplay'] = false;
 
 // api for templates
-function multieditDisplay($index) {
-	if ($GLOBALS['multiEditDisplay'] === false) {
+function multieditDisplay($index,$return = null) {
+	//if ($GLOBALS['multiEditDisplay'] === false) {
 		$GLOBALS['multiEditDisplay'] = get_post_custom(0);
-	}
+	//}
 	$index = "multiedit_$index";	
 
+	
 	if (isset($GLOBALS['multiEditDisplay'][$index])) {
-		echo $GLOBALS['multiEditDisplay'][$index][0];
+		 //check to apply filters is Shortcode support option is on.
+		if( get_option('pagely_multiedit_shortcodes') == 1 ) {
+			$me_str = apply_filters('the_content',$GLOBALS['multiEditDisplay'][$index][0]);
+		} else {
+			$me_str = $GLOBALS['multiEditDisplay'][$index][0];
+		}
+		// clean out some random mce charcters, this could be improved or we can better sanitize on the save side
+		$me_str = preg_replace( '#mce_[a-z]+="[^"]+"#', '', $me_str );
+		
+		if ($return) {
+			return $me_str;
+		} else {
+			echo $me_str;
+		}
 	}
 
 }
@@ -75,8 +151,7 @@ function drawMultieditHTML($meta,$presentregions) {
 	echo '<div id="multiEditControl"></div>';
 	echo '<div id="multiEditHidden"><span class="multieditbutton selected" id="default">Main Content</span>';
 
-		//print_r($meta);
-		//print_r($presentregions);
+		
 		
 	// this adds the multiedit tabs that appear above the tinymce editor
 		if (is_array($meta)) {
@@ -84,7 +159,7 @@ function drawMultieditHTML($meta,$presentregions) {
 				if (preg_match('/^multiedit_(.+)/',$item['meta_key'],$matches)) {
 					// lets check regions defined in this template ($presentregions) against those in meta
 					// so we can treat meta values that may be in $post, but not in this template differently
-					print_r($matches);
+					//print_r($matches);
 					$notactive = false;
 					if (!array_key_exists($matches[1],$presentregions)) { $notactive = 'notactive'; $fields[] = $matches[1];}
 					 $mkey = trim($item['meta_key']);
@@ -99,7 +174,7 @@ function drawMultieditHTML($meta,$presentregions) {
 			if (!empty($fields)) {echo "<div id='nonactive' style='display:none'><p>".implode(', ',$fields)." region(s) are not declared in the template.</p></div>";}
 		}
 	
-	echo "<div id='multiEditFreezer' style='display:none'>".$post->post_content."</div></div>\n";
+	echo "<div id='multiEditFreezer' style='display:none'>". $post->post_content ."</div></div>\n";
 }
 
 function doMultiMeta() {
@@ -111,36 +186,41 @@ function doMultiMeta() {
 	$post->page_template == 'default' ? $post->page_template = 'page.php' : '';
 	
 	$templatefile = locate_template(array($post->page_template));	
-	$template_data = implode('', array_slice(file($templatefile), 0, 10));	
-	$matches = '';
-	//check for multiedit declaration in template
-	if (preg_match( '|MultiEdit:(.*)$|mi', $template_data, $matches)) {
-		 $multi = explode(',',_cleanup_header_comment($matches[1]));
-		 // load scripts
-		 multieditAdminHeader();
-		 // WE have multiedit zones, load js and css load
-		 add_action ('edit_page_form', 'multieditAdminEditor', 1);
-		 add_action ('edit_form_advanced', 'multieditAdminEditor', 1);
-		 
-		 //simple var assigment
-		 foreach($meta as $k=>$v) {
-		 	 foreach($multi as $region) {
-		 	  	if (in_array('multiedit_'.$region,$v)) {
-		 	  		$present[$region] = true;
-		 	  	}
-		 	 }
-		 }
-		
-		//draw html
-		drawMultieditHTML($meta,$present);
-
-		// if custom field is not declared yet, create one with update_post_meta 
-		foreach($multi as $region) {
-			if(!isset($present[$region])) {
-					update_post_meta($post->ID, 'multiedit_'.$region, '');
-			} 
-		}		 
-	} // end preg_match
+	if(file_exists($templatefile)) {
+		$template_data = implode('', array_slice(file($templatefile), 0, 10));	
+		$matches = '';
+		//check for multiedit declaration in template
+		if (preg_match( '|MultiEdit:(.*)$|mi', $template_data, $matches)) {
+			 $multi = explode(',',_cleanup_header_comment($matches[1]));
+			 // load scripts
+			 multieditAdminHeader();
+			 // WE have multiedit zones, load js and css load
+			 add_action ('edit_page_form', 'multieditAdminEditor', 1);
+			 add_action ('edit_form_advanced', 'multieditAdminEditor', 1);
+			 
+			 //simple var assigment
+			 foreach($meta as $k=>$v) {
+			 	 foreach($multi as $region) {
+			 	  	if (in_array('multiedit_'.$region,$v)) {
+			 	  		$present[$region] = true;
+			 	  	}
+			 	 }
+			 }
+			
+			//draw html
+			drawMultieditHTML($meta,$present);
+	
+			// if custom field is not declared yet, create one with update_post_meta 
+			foreach($multi as $region) {
+				if(!isset($present[$region])) {
+						update_post_meta($post->ID, 'multiedit_'.$region, '');
+				} 
+			}		 
+		} // end preg_match
+	} else {
+		// cant find a suitable template.. display nothing. Content is still in custom fields, so it is not lost.
+		return false;
+	}
 				 
 }
-
+?>
